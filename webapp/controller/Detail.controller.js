35ca77cf2709e7ee2d8f392f923b53f2fb0a037d
Katsuraji1 <verahovets@gmail.com>
2023-05-27 18:41:42 +0300

    implemented routing

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -10,78 +10,29 @@ sap.ui.define([
 
 			formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
+			_oViewModel: new JSONModel({
+				masterItem: ""
+			}),
 
 			onInit : function () {
-				// Model used to manipulate control states. The chosen values make sure,
-				// detail page is busy indication immediately so there is no break in
-				// between the busy indication for loading the view's meta data
-				var oViewModel = new JSONModel({
-					busy : false,
-					delay : 0
-				});
 
 				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
-
-				this.setModel(oViewModel, "detailView");
-
-				this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
-			},
-
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
-
-			/**
-			 * Event handler when the share by E-Mail button has been clicked
-			 * @public
-			 */
-			onShareEmailPress : function () {
-				var oViewModel = this.getModel("detailView");
-
-				sap.m.URLHelper.triggerEmail(
-					null,
-					oViewModel.getProperty("/shareSendEmailSubject"),
-					oViewModel.getProperty("/shareSendEmailMessage")
-				);
+				this.setModel(this._oViewModel, "detailView")
 			},
 
 
-
-			/* =========================================================== */
-			/* begin: internal methods                                     */
-			/* =========================================================== */
-
-			/**
-			 * Binds the view to the object path and expands the aggregated line items.
-			 * @function
-			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-			 * @private
-			 */
 			_onObjectMatched : function (oEvent) {
-				var sObjectId =  oEvent.getParameter("arguments").objectId;
-				this.getModel().metadataLoaded().then( function() {
-					var sObjectPath = this.getModel().createKey("zjblessons_base_Headers", {
-						HeaderID :  sObjectId
-					});
-					this._bindView("/" + sObjectPath);
-				}.bind(this));
+				const sEntity = oEvent.getParameter('arguments').entity;
+
+				this.getModel('detailView').setProperty('/masterItem', sEntity);
+
+				
 			},
 
-			/**
-			 * Binds the view to the object path. Makes sure that detail view displays
-			 * a busy indicator while data for the corresponding element binding is loaded.
-			 * @function
-			 * @param {string} sObjectPath path to the object to be bound to the view.
-			 * @private
-			 */
+
 			_bindView : function (sObjectPath) {
-				// Set busy indicator during view binding
 				var oViewModel = this.getModel("detailView");
 
-				// If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
 				oViewModel.setProperty("/busy", false);
 
 				this.getView().bindElement({
@@ -102,11 +53,8 @@ sap.ui.define([
 				var oView = this.getView(),
 					oElementBinding = oView.getElementBinding();
 
-				// No data for the binding
 				if (!oElementBinding.getBoundContext()) {
 					this.getRouter().getTargets().display("detailObjectNotFound");
-					// if object could not be found, the selection in the master list
-					// does not make sense anymore.
 					this.getOwnerComponent().oListSelector.clearMasterListSelection();
 					return;
 				}
@@ -127,17 +75,10 @@ sap.ui.define([
 			},
 
 			_onMetadataLoaded : function () {
-				// Store original busy indicator delay for the detail view
 				var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
 					oViewModel = this.getModel("detailView");
-
-				// Make sure busy indicator is displayed immediately when
-				// detail view is displayed for the first time
 				oViewModel.setProperty("/delay", 0);
-
-				// Binding the view will set it to not busy - so the view is always busy if it is not bound
 				oViewModel.setProperty("/busy", true);
-				// Restore original busy indicator delay for the detail view
 				oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
 			}
 
