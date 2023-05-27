85e5e5be581db540253016c3a6d304c6b13fe257
Katsuraji1 <verahovets@gmail.com>
2023-05-27 17:06:49 +0300

    masterDetail init

---
+++ webapp/test/integration/pages/Browser.js
@@ -0,0 +1,82 @@
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"zjblessons/MasterDetail/test/integration/pages/Common"
+], function(Opa5, Common) {
+	"use strict";
+
+	Opa5.createPageObjects({
+		onTheBrowserPage : {
+			baseClass : Common,
+
+			actions : {
+
+				iChangeTheHashToObjectN : function (iObjIndex) {
+					return this.waitFor(this.createAWaitForAnEntitySet({
+						entitySet : "Objects",
+						success : function (aEntitySet) {
+							Opa5.getHashChanger().setHash("/zjblessons_base_Headers/" + aEntitySet[iObjIndex].HeaderID);
+						}
+					}));
+				},
+
+				iChangeTheHashToTheRememberedItem : function () {
+					return this.waitFor({
+						success : function () {
+							var sObjectId = this.getContext().currentItem.id;
+							Opa5.getHashChanger().setHash("/zjblessons_base_Headers/" + sObjectId);
+						}
+					});
+				},
+
+				iChangeTheHashToSomethingInvalid : function () {
+					return this.waitFor({
+						success : function () {
+							Opa5.getHashChanger().setHash("/somethingInvalid");
+						}
+					});
+				}
+
+			},
+
+			assertions : {
+
+				iShouldSeeTheHashForObjectN : function (iObjIndex) {
+					return this.waitFor(this.createAWaitForAnEntitySet({
+						entitySet : "Objects",
+						success : function (aEntitySet) {
+							var oHashChanger = Opa5.getHashChanger(),
+								sHash = oHashChanger.getHash();
+							Opa5.assert.strictEqual(sHash, "zjblessons_base_Headers/" + aEntitySet[iObjIndex].HeaderID, "The Hash is not correct");
+						}
+					}));
+				},
+
+				iShouldSeeTheHashForTheRememberedObject : function () {
+					return this.waitFor({
+						success : function () {
+							var sObjectId = this.getContext().currentItem.id,
+								oHashChanger = Opa5.getHashChanger(),
+								sHash = oHashChanger.getHash();
+							Opa5.assert.strictEqual(sHash, "zjblessons_base_Headers/" + sObjectId, "The Hash is not correct");
+						}
+					});
+				},
+
+				iShouldSeeAnEmptyHash : function () {
+					return this.waitFor({
+						success : function () {
+							var oHashChanger = Opa5.getHashChanger(),
+								sHash = oHashChanger.getHash();
+							Opa5.assert.strictEqual(sHash, "", "The Hash should be empty");
+						},
+						errorMessage : "The Hash is not Correct!"
+					});
+				}
+
+			}
+
+		}
+
+	});
+
+});
