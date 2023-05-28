05253595fcec3b7b0e66d38acbdb4482f4b84151
Katsuraji1 <verahovets@gmail.com>
2023-05-28 16:07:51 +0300

    fixed messageToast

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -84,22 +84,24 @@ sap.ui.define([
 			},
 
 			onPressSaveEditMaterial: function(){
-				this.getModel().submitChanges({
-					success: function() {
-						sap.m.MessageToast.show('Record successfully changed!',{
-							duration: 1000,
-							animationTimingFunction: "ease",
-							animationDuration: 1000, 
-						})
-					},
-					error: function(oError){
-						sap.m.MessageToast.show('Error!',{
-							duration: 1000,
-							animationTimingFunction: "ease",
-							animationDuration: 1000, 
-						})
-					},
-				});
+				if(this.getModel().hasPendingChanges()){
+					this.getModel().submitChanges({
+						success: function() {
+							sap.m.MessageToast.show('Record successfully changed!',{
+								duration: 1000,
+								animationTimingFunction: "ease",
+								animationDuration: 1000, 
+							})
+						},
+						error: function(oError){
+							sap.m.MessageToast.show('Error!',{
+								duration: 1000,
+								animationTimingFunction: "ease",
+								animationDuration: 1000, 
+							})
+						},
+					});
+				}
 				this._setEditMode(false);
 			},
 
