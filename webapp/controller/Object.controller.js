d63cad1c92d7fdd0d0d28ff97da214cc4eb5cf91
Katsuraji1 <verahovets@gmail.com>
2023-06-09 15:34:48 +0300

    eventBus

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -16,83 +16,25 @@ sap.ui.define([
 
 			formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
-
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
 			onInit : function () {
-				// Model used to manipulate control states. The chosen values make sure,
-				// detail page is busy indication immediately so there is no break in
-				// between the busy indication for loading the view's meta data
 				var iOriginalBusyDelay,
 					oViewModel = new JSONModel({
 						busy : true,
 						delay : 0
 					});
 
-				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
-
-				// Store original busy indicator delay, so it can be restored later on
 				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
 				this.setModel(oViewModel, "objectView");
 				this.getOwnerComponent().getModel().metadataLoaded().then(function () {
-						// Restore original busy indicator delay for the object view
 						oViewModel.setProperty("/delay", iOriginalBusyDelay);
 					}
 				);
+				const bus = this.getOwnerComponent().getEventBus()
+				bus.subscribe('channelA', 'orderSelected', this.bindData, this)
+				/* this.oModel = new sap.ui.model.odata.v2.ODataModel('/Material')
+				this.oModel.setDeferredGroups(['material', 'description']) */
 			},
 
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
-
-
-			/**
-			 * Event handler  for navigating back.
-			 * It there is a history entry we go one step back in the browser history
-			 * If not, it will replace the current entry of the browser history with the worklist route.
-			 * @public
-			 */
-			onNavBack : function() {
-				var sPreviousHash = History.getInstance().getPreviousHash();
-
-				if (sPreviousHash !== undefined) {
-					history.go(-1);
-				} else {
-					this.getRouter().navTo("worklist", {}, true);
-				}
-			},
-
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
-
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-			 * @private
-			 */
-			_onObjectMatched : function (oEvent) {
-				var sObjectId =  oEvent.getParameter("arguments").objectId;
-				this.getModel().metadataLoaded().then( function() {
-					var sObjectPath = this.getModel().createKey("zjblessons_base_Materials", {
-						MaterialID :  sObjectId
-					});
-					this._bindView("/" + sObjectPath);
-				}.bind(this));
-			},
-
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {string} sObjectPath path to the object to be bound
-			 * @private
-			 */
 			_bindView : function (sObjectPath) {
 				var oViewModel = this.getModel("objectView"),
 					oDataModel = this.getModel();
@@ -103,10 +45,6 @@ sap.ui.define([
 						change: this._onBindingChange.bind(this),
 						dataRequested: function () {
 							oDataModel.metadataLoaded().then(function () {
-								// Busy indicator on view should only be set if metadata is loaded,
-								// otherwise there may be two busy indications next to each other on the
-								// screen. This happens because route matched handler already calls '_bindView'
-								// while metadata is loaded.
 								oViewModel.setProperty("/busy", true);
 							});
 						},
@@ -117,12 +55,12 @@ sap.ui.define([
 				});
 			},
 
+
 			_onBindingChange : function () {
 				var oView = this.getView(),
 					oViewModel = this.getModel("objectView"),
 					oElementBinding = oView.getElementBinding();
 
-				// No data for the binding
 				if (!oElementBinding.getBoundContext()) {
 					this.getRouter().getTargets().display("objectNotFound");
 					return;
@@ -134,12 +72,25 @@ sap.ui.define([
 					sObjectName = oObject.MaterialText;
 
 				oViewModel.setProperty("/busy", false);
+			},
 
-				oViewModel.setProperty("/shareSendEmailSubject",
-				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-				oViewModel.setProperty("/shareSendEmailMessage",
-				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
-			}
+			bindData: function(channelID, eventID, parametersMap){
+				const sObjectId =  parametersMap.objectId;
+				this.getModel().metadataLoaded().then( function() {
+					var sObjectPath = this.getModel().createKey("zjblessons_base_Materials", {
+						MaterialID :  sObjectId
+					});
+					this._bindView("/" + sObjectPath);
+				}.bind(this));
+			},
+
+			onPressSubmitMaterialText: function(oEvent){
+				
+			},
+
+			onPressSubmitMateriaDescription: function(oEvent){
+
+			},
 
 		});
 
