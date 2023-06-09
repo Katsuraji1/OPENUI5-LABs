6606136ae1e71713dd848c10c1e8d598c9c6133f
Katsuraji1 <verahovets@gmail.com>
2023-06-09 17:00:27 +0300

    implemented binding and busy indicator

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -15,46 +15,59 @@ sap.ui.define([
 	return BaseController.extend("zjblessons.Worklist.controller.List", {
         formatter: formatter,
         onInit : function () {
-            var iOriginalBusyDelay,
-                oViewModel = new JSONModel({
-                    busy : true,
+            var oList = this.byId("list"),
+            iOriginalBusyDelay = oList.getBusyIndicatorDelay(),
+            oViewModel = new JSONModel({
                     delay : 0,
+                    busy: true,
                 });
 
             this.getRouter().getRoute("list").attachPatternMatched(this._onObjectMatched, this);
 
-            iOriginalBusyDelay = this.getView().setBusyIndicatorDelay(0).getBusyIndicatorDelay();
+            iOriginalBusyDelay = this.getView().setBusyIndicatorDelay(0);
             this.setModel(oViewModel, "listView");
             this.getOwnerComponent().getModel().metadataLoaded().then(function () {
                     oViewModel.setProperty("/delay", iOriginalBusyDelay);
                 }
             );
+            oList.attachEventOnce("updateFinished", function(){
+                this.getModel('listView').setProperty('/busy', false) 
+            });
         },
 
         _onObjectMatched : function () {
-            this.getModel().read('/zjblessons_base_Materials', {
-                success: (oData) => {
-                    this._setListData(oData);
-                }
-            });
+            try {
+                this.getModel().read('/zjblessons_base_Materials', {
+                    success: (oData) => {
+                        this._setListData(oData);
+                    },
+                    error: (oError) => {
+                        new sap.m.MessageToast.show(oError.message)
+                    }
+                });
+            } catch(oError){
+                debugger;
+                new sap.m.MessageToast.show(oError.toString())
+            } finally{
+                //отклбчение busy индикатора в "updateFinished", тк finally отрабатывает до биндинга таблицы
+            }
         },
 
         _setListData: function(oData){
             
             const oList = this.getView().byId('list');
-            var oModel = new sap.ui.model.json.JSONModel(oData);
-            debugger;
-            oModel.setData({list: oData});    	  
+            var oModel = new sap.ui.model.json.JSONModel();
+            oModel.setData(oData);    	  
             oList.setModel(oModel);
             oList.bindItems(
                 {
-                    path: '/list',
+                    path: '/results',
                     template: new sap.m.StandardListItem({
                         title: "{MaterialText}",
                         description: "{MaterialDescription}"
                     })
             }
-            ); 
+            );
         }
 	});
 });
